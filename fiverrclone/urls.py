69c64d8b4c3d4ea17defafea2a4354d42db2a83f
Gregory Griffin <mr.gregory.griffin@gmail.com>
2017-01-15 21:08:36 +0100

    Add templates, static resources and URL name

--- fiverrclone/urls.py
+++ fiverrclone/urls.py
@@ -20,4 +20,5 @@ from fiverrcloneapp import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.home, name='home'),
+    url(r'^gigs/(?P<id>[0-9]+)/$', views.gig_detail, name='gig_detail')
 ]
