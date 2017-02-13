780a015c4d8e7ccc04c6e8a688f94d2e91725285
Gregory Griffin <mr.gregory.griffin@gmail.com>
2017-02-13 16:51:23 +0100

    Configure Database

--- fiverrclone/settings.py
+++ fiverrclone/settings.py
@@ -135,3 +135,8 @@ LOGIN_REDIRECT_URL= '/'
 
 SOCIAL_AUTH_FACEBOOK_KEY = '396237804050198'
 SOCIAL_AUTH_FACEBOOK_SECRET = '4b6fabc9497c73591240e086dcb2f34d'
+
+# Replace database setting to use postgresql on Heroku
+import dj_database_url
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
